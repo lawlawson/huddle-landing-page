3d34d6ca6524f34b309537a96b68f8de300e0022
Lawrence <lawrence.narh-lawson@boomin.com>
2021-09-25 23:32:42 +0100

    Added images and updated favicon

--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -9,7 +9,7 @@ const GlobalStyles = createGlobalStyle`
 }
 
 body {
-  background: ${(theme) => theme.colors.body};
+  background: ${({ theme }) => theme.colors.body};
   color: hsl(192, 100%, 9%);
   font-family: 'Poppins', sans-serif;
   font-size: 1.15em;
