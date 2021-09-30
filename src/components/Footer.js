1f111ed88d8314e9e7c4891d34f9ebf4e8759b30
Lawrence <lawrence.narh-lawson@boomin.com>
2021-09-30 23:10:58 +0100

    Added Footer file

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import { Container } from './styles/Container.styled';
+import { Flex } from './styles/Flex.styled';
+
+export default function Footer() {
+  return (
+    <div>
+      <Container>
+        <img src='./images/logo_white.svg' alt='' />
+        <Flex></Flex>
+      </Container>
+    </div>
+  );
+}
