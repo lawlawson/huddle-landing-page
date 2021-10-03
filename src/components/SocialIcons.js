3b13e90c9ef581636394afa1c6a73126dab014be
Lawrence <lawrence.narh-lawson@boomin.com>
2021-10-03 18:51:43 +0100

    Added links to social icons

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,3 +1,23 @@
+import { FaTwitter, FaFacebook, FaLinkedin } from 'react-icons/fa';
+
 export default function SocialIcons() {
-  return <div></div>;
+  return (
+    <div>
+      <li>
+        <a href='https://github.com/lawlawson'>
+          <FaTwitter />
+        </a>
+      </li>
+      <li>
+        <a href='https://github.com/lawlawson'>
+          <FaFacebook />
+        </a>
+      </li>
+      <li>
+        <a href='https://github.com/lawlawson'>
+          <FaLinkedin />
+        </a>
+      </li>
+    </div>
+  );
 }
