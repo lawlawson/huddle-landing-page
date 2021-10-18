681e30653997c0be4a63f8ff801bde372d6d24a0
Lawrence <lawrence.narh-lawson@boomin.com>
2021-10-19 00:06:36 +0100

    Styled button in header

--- src/components/styles/Button.styled.js
+++ src/components/styles/Button.styled.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
   border-radius: 50px;
-  border: none;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  border: 1px solid #ff52c1;
+  /* box-shadow: 0 0 10px rgba(0, 0, 0, 0.15); */
   cursor: pointer;
   font-size: 16px;
   font-weight: 700;
   padding: 15px 60px;
   background-color: ${({ bg }) => bg || '#fff'};
-  color: ${({ color }) => color || '#333'};
+  color: ${({ color }) => color || '#FF52C1'};
 
   &:hover {
     opacity: 0.9;
